61cf35de341f652f44d31a7b30594ddfce843b3c
abrarimamsatria09 <092004abrar@gmail.com>
2023-03-23 11:22:38 +0700

    membuat procedure display

--- belajar insertion sort/belajar insertion sort.cpp
+++ belajar insertion sort/belajar insertion sort.cpp
@@ -57,3 +57,16 @@ void inserionsort() {							//procedure insertionsort
 
 	}
 }
+
+void display() {												//procedure display
+	cout << endl;												//output baris kosong
+	cout << "\n==================================" << endl;		//output ke layar
+	cout << "element array yang telah tersusun" << endl;		//output ke layar
+	cout << "==================================" << endl;		//output ke layar
+
+	for (int j = 0; j < n; j++) {		//looping dengan j dimulai dari 0 hingga n-1
+		cout << arr[j] << endl;			//output ke layar
+	}
+	cout << endl;		//ouput baris kosong
+}
+
