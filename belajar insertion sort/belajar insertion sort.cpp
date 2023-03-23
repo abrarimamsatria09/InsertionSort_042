c8619bde605986874eda58f20936c55fbf2d196d
abrarimamsatria09 <092004abrar@gmail.com>
2023-03-23 10:36:25 +0700

    membuat inputan jumlah element array

--- belajar insertion sort/belajar insertion sort.cpp
+++ belajar insertion sort/belajar insertion sort.cpp
@@ -3,3 +3,11 @@ using namespace std;
 
 int arr[20];		//membuat array dengan panjang data 20
 int n;				//membuat variabel inputan n
+
+void input() {		//procedure input
+	while (true)
+	{
+		cout << "masukkan jumlah data pada array : ";	//membuat inputan jumlah element array
+		cin >> n;										//memanggil variable inputan n
+	}
+}
