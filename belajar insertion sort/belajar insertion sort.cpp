2664af372684f69781fe418f17e324ab7b248ca7
abrarimamsatria09 <092004abrar@gmail.com>
2023-03-23 10:50:24 +0700

    membuat procedure input

--- belajar insertion sort/belajar insertion sort.cpp
+++ belajar insertion sort/belajar insertion sort.cpp
@@ -18,4 +18,14 @@ void input() {		//procedure input
 			cout << "\narray yang anda masukkan maksimal 20 elemen. \n";	//menampilkan pesan jika data lebih dari 20
 		}
 	}
+	cout << endl;									//membuat jarak per baris program
+	cout << "======================" << endl;		//membuat tampilan susunan data element array
+	cout << "masukkan element array" << endl;
+	cout << "======================" << endl;
+
+	for (int i = 0; i < n; i++)						//menggunakan perulangan for untuk menyimpan data pada array
+	{
+		cout << "dpata ke-" << (i + 1) << ": ";		//memasukkan atau menginputkan nilai data n
+		cin >> arr[i];								//menyimpan nilai data n kedalam array arr
+	}
 }
